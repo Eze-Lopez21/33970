db18a681c7c6cd78d81a2f39d43e2d5e99c910d9
Ezequiel Lopez <eze_lopez21@hotmail.com>
2022-08-09 17:44:48 -0300

    primera entrega desafio final

--- script.js
+++ script.js
@@ -1,38 +1,107 @@
-const convertirD = (num1, num2) => num1 / num2
+const productosAsignados = [
+    {
+        numero: '1',
+        nombre: 'Combo #1',
+        precio: 100,
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    {
+        numero: '2',
+        nombre: 'Combo #2',
+        precio: 200,
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    {
+        numero: '3',
+        nombre: 'Combo #3',
+        precio: 300,
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    {
+        numero: '4',
+        nombre: 'Combo #4',
+        precio: 400,
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    {
+        numero: '5',
+        nombre: 'Combo #5',
+        precio: 500,
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    {
+        numero: '6',
+        nombre: 'Combo #6',
+        precio: 600,
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+];
 
-const convertirP = (num1, num2) => num1 * num2
-
-
-function conversiones(operacion, num1, num2) {
-    switch(operacion) {
-        case "p":
-            console.log(convertirD(num1, num2))
-            break
-        case "d":
-            console.log(convertirP(num1, num2))
-            break
-        default:
-            console.log("Opcion no valida")
+// Clase
+class Producto{
+    constructor(numero, nombre, precio, descripcion){
+        this.numero = numero;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripcion = descripcion;
+    }
+    obtenerInfo(){
+        return `${this.numero} : ${this.nombre} ( $${this.precio} )`;
+    }
+    obtenerAviso(){
+        return `El ${this.nombre} tiene un valor de $${this.precio}`;
     }
 }
-let respuesta
+// Variable
+const carrito = [];
+// Funciones
+const obtenerInfoProductos = (productosArray) => {
+    return productosArray.map( (elemento)=>  elemento.obtenerInfo() ).join('\n');
+}
+const agregarAlCarritoPorNumero = (productos) => {
+    const infoProductos = obtenerInfoProductos(productos);
+    const numero = prompt('Ingrese el numero del combo que desea agregar al carrito:\n' + infoProductos);
+    const producto = productos.find((producto) => producto.numero === numero);
+    if (!producto) return alert('El numero que ingresó, no corresponde a ningún combo.');
+    carrito.push(producto);
+    alert('Producto agregado al carrito');
+}
+const imprimirCarrito = (carritoDeProductos) => {
+    carritoDeProductos.forEach((producto) => {
+        console.log(producto.obtenerAviso());
+    });
+}
+const obtenerTotal = (productosArray) => {
+    let total = 0;
+    productosArray.forEach((producto) => {
+        total += producto.precio;
+    });
+    return total;
+}
+// Ejecución
+const productos = productosAsignados.map(producto => new Producto(
+    producto.numero,
+    producto.nombre,
+    producto.precio,
+    producto.descripcion,
+));
+
+let confirmacion
+
+// Bucle
 do {
-    let num1, num2, operacion
-    do {
-        operacion = prompt("Ingrese la moneda a convertir (d = dolares, p = pesos argentinos)").toLowerCase()
-        num1 = prompt("Ingrese su cantidad a convertir")
-        num2 = 130.62
-    
-        if(isNaN(num1)) {
-            console.log("El numero ingresado no es valido")
-        }
-    
-    } while((isNaN(num1)))
-    
-    conversiones(operacion, num1 ,num2)
+    agregarAlCarritoPorNumero(productos);
+    confirmacion = prompt('¿Desea agregar otro combo? (si | no)').toLowerCase()
+} while(confirmacion == 'si')
 
-    do {
-        respuesta = prompt("¿Desea realizar otra conversion? (si, no)").toLowerCase()
-    } while(respuesta != "si" && respuesta != "no")
-
-}while(respuesta != "no")
+// Condicional
+if (confirmacion == 'no') {
+    alert('Abre la consola para ver el total de tu compra');
+imprimirCarrito(carrito);
+console.log('TOTAL: ' , obtenerTotal(carrito));
+}
+else if (confirmacion != 'no') {
+    alert('Su respuesta no es valida. Intente nuevamente');
+    imprimirCarrito(carrito);
+    console.log('TOTAL: ' , obtenerTotal(carrito));
+}
