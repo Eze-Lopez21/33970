2ce1253d2fcf0aecbd96eeb39de65d21951a65fe
Ezequiel Lopez <106112487+Eze-Lopez21@users.noreply.github.com>
2022-08-16 22:23:07 -0300

    nuevo desafio

--- script.js
+++ script.js
@@ -1,107 +1,46 @@
-const productosAsignados = [
-    {
-        numero: '1',
-        nombre: 'Combo #1',
-        precio: 100,
-        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
-    {
-        numero: '2',
-        nombre: 'Combo #2',
-        precio: 200,
-        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
-    {
-        numero: '3',
-        nombre: 'Combo #3',
-        precio: 300,
-        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
-    {
-        numero: '4',
-        nombre: 'Combo #4',
-        precio: 400,
-        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
-    {
-        numero: '5',
-        nombre: 'Combo #5',
-        precio: 500,
-        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
-    {
-        numero: '6',
-        nombre: 'Combo #6',
-        precio: 600,
-        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
-];
-
-// Clase
-class Producto{
-    constructor(numero, nombre, precio, descripcion){
-        this.numero = numero;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.descripcion = descripcion;
-    }
-    obtenerInfo(){
-        return `${this.numero} : ${this.nombre} ( $${this.precio} )`;
-    }
-    obtenerAviso(){
-        return `El ${this.nombre} tiene un valor de $${this.precio}`;
+class User {
+    constructor(username, number, email, password) {
+        this.username = username
+        this.number = number
+        this.email = email
+        this.password = password
     }
 }
-// Variable
-const carrito = [];
-// Funciones
-const obtenerInfoProductos = (productosArray) => {
-    return productosArray.map( (elemento)=>  elemento.obtenerInfo() ).join('\n');
-}
-const agregarAlCarritoPorNumero = (productos) => {
-    const infoProductos = obtenerInfoProductos(productos);
-    const numero = prompt('Ingrese el numero del combo que desea agregar al carrito:\n' + infoProductos);
-    const producto = productos.find((producto) => producto.numero === numero);
-    if (!producto) return alert('El numero que ingresó, no corresponde a ningún combo.');
-    carrito.push(producto);
-    alert('Producto agregado al carrito');
-}
-const imprimirCarrito = (carritoDeProductos) => {
-    carritoDeProductos.forEach((producto) => {
-        console.log(producto.obtenerAviso());
-    });
-}
-const obtenerTotal = (productosArray) => {
-    let total = 0;
-    productosArray.forEach((producto) => {
-        total += producto.precio;
-    });
-    return total;
-}
-// Ejecución
-const productos = productosAsignados.map(producto => new Producto(
-    producto.numero,
-    producto.nombre,
-    producto.precio,
-    producto.descripcion,
-));
 
-let confirmacion
+const idForm = document.getElementById("idForm")
+const botonUsers = document.getElementById("botonUsers")
+const divUsers = document.getElementById("divUsers")
 
-// Bucle
-do {
-    agregarAlCarritoPorNumero(productos);
-    confirmacion = prompt('¿Desea agregar otro combo? (si | no)').toLowerCase()
-} while(confirmacion == 'si')
+const users = []
 
-// Condicional
-if (confirmacion == 'no') {
-    alert('Abre la consola para ver el total de tu compra');
-imprimirCarrito(carrito);
-console.log('TOTAL: ' , obtenerTotal(carrito));
-}
-else if (confirmacion != 'no') {
-    alert('Su respuesta no es valida. Intente nuevamente');
-    imprimirCarrito(carrito);
-    console.log('TOTAL: ' , obtenerTotal(carrito));
-}
+idForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const username = document.getElementById("username").value
+    const number = document.getElementById("number").value
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+
+    const user = new User(username, number, email, password)
+
+    users.push(user)
+
+    idForm.reset()
+    console.log(users)
+})
+
+
+botonUsers.addEventListener('click', () => {
+    divUsers.innerHTML = ""
+    users.forEach((user, indice) => {
+        divUsers.innerHTML += `
+            <div class="card" id="user${indice}" style="width: 18rem; margin:3px;">
+                <img src="https://static.vecteezy.com/system/resources/previews/005/276/776/non_2x/logo-icon-person-on-white-background-free-vector.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">${user.username}</h5>
+                <p class="card-text">${user.number} años</p>
+                <p class="card-text">${user.email}</p>
+                </div>
+            </div>
+        `
+    })
+})
