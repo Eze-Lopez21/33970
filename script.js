3d06fecdb1f6557d24f46b866d39bd06f4a993cf
Ezequiel Lopez <eze_lopez21@hotmail.com>
2022-08-23 15:40:41 -0300

    ultimos detalles

--- script.js
+++ script.js
@@ -1,46 +1,67 @@
-class User {
-    constructor(username, number, email, password) {
-        this.username = username
-        this.number = number
-        this.email = email
-        this.password = password
+class Consulta {
+    constructor(nombre, apellido, correo, direccion,comentarios) {
+        this.nombre = nombre
+        this.apellido = apellido
+        this.correo = correo
+        this.direccion = direccion
+        this.comentarios = comentarios
     }
 }
 
-const idForm = document.getElementById("idForm")
-const botonUsers = document.getElementById("botonUsers")
-const divUsers = document.getElementById("divUsers")
+let consultas = []
 
-const users = []
+if(localStorage.getItem('consultas')) {
+    consultas =  JSON.parse(localStorage.getItem('consultas'))
+} else {
+    localStorage.setItem('consultas', JSON.stringify(consultas))
+}
 
-idForm.addEventListener('submit', (event) => {
-    event.preventDefault()
-    const username = document.getElementById("username").value
-    const number = document.getElementById("number").value
-    const email = document.getElementById("email").value
-    const password = document.getElementById("password").value
+const form = document.getElementById("main__form")
+const botonConsultas = document.getElementById("boton__consultas")
+const zonaConsultas = document.getElementById("zona__consultas")
 
-    const user = new User(username, number, email, password)
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const datForm = new FormData(e.target)
 
-    users.push(user)
+    const consulta = new Consulta(datForm.get("nombre"), datForm.get("apellido"), datForm.get("correo"), datForm.get("direccion"), datForm.get("comentarios"))
+    
+    consultas.push(consulta)
 
-    idForm.reset()
-    console.log(users)
+    localStorage.setItem('consultas', JSON.stringify(consultas))
+
+    form.reset()
 })
 
+boton__consultas.addEventListener('click', () => {
+    const tarStorage = JSON.parse(localStorage.getItem('consultas'))
 
-botonUsers.addEventListener('click', () => {
-    divUsers.innerHTML = ""
-    users.forEach((user, indice) => {
-        divUsers.innerHTML += `
-            <div class="card" id="user${indice}" style="width: 18rem; margin:3px;">
-                <img src="https://static.vecteezy.com/system/resources/previews/005/276/776/non_2x/logo-icon-person-on-white-background-free-vector.jpg" class="card-img-top" alt="...">
+    zona__consultas.innerHTML = ""
+
+    tarStorage.forEach((consulta, indice) => {
+        zona__consultas.innerHTML += `
+            <div class="card bg-warning m-5" id="consulta${indice}" style="width: 20rem;">
+                <div class="card-header">
+                    <h2>${consulta.nombre} ${consulta.apellido}<h2>
+                </div>
                 <div class="card-body">
-                <h5 class="card-title">${user.username}</h5>
-                <p class="card-text">${user.number} años</p>
-                <p class="card-text">${user.email}</p>
+                    <h5 class="card-title fw-bold"><i class="bi bi-envelope"></i> ${consulta.correo}</h5>
+                    <p class="card-text bold"><i class="bi bi-house-door"></i> ${consulta.direccion}</p>
+                    <p class="card-text"><i class="bi bi-pencil-square"></i> ${consulta.comentarios}</p>
+                    <button class="btn btn-danger">Eliminar</button>
                 </div>
             </div>
         `
+    })
+
+    tarStorage.forEach((consulta, indice) => {
+        const infoConsulta = document.getElementById(`consulta${indice}`)
+
+        infoConsulta.children[1].children[3].addEventListener('click', () => {
+            infoConsulta.remove() //DOM
+            consultas.splice(indice, 1) //Array
+            localStorage.setItem('consultas', JSON.stringify(consultas)) //Local storage
+            console.log(`${consulta.nombre} Eliminada`)
+        })
     })
 })
